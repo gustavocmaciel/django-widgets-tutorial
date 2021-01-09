4235b88e024aabc8641e6eeb3a95bc5199db5cb7
gcmaciel <gustavocmaciel2@gmail.com>
2021-01-09 12:36:31 -0300

    Add form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import forms
+from django import forms
+
+# Import the User model
+##from .models import User
+
+class UserInfoForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+
 # Create your views here.
 
 def index(request):
-    return render(request, "app/index.html"), {
-            "form": UserInfoForm()
+    if request.method == "GET":
+        return render(request, "app/index.html", {
+                "form": UserInfoForm,
+            })
