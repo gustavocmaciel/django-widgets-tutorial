94bbfa949a8eb51a1070190353793b7693e54edd
gcmaciel <gustavocmaciel2@gmail.com>
2021-01-09 14:34:44 -0300

    Create user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
--- app/views.py
+++ app/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 
 # Import forms
 from django import forms
+from django.forms import ModelForm, TextInput, EmailInput 
 
 # Import the User model
 ##from .models import User
 
 class UserInfoForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;', 'class': 'form-control'}))
 
 # Create your views here.
 
