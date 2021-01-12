dcdeba8cc745841162dbf9a52b998f0c01011f9e
gcmaciel <gustavocmaciel2@gmail.com>
2021-01-12 09:43:30 -0300

    Create form

--- app/views.py
+++ app/views.py
@@ -1,27 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django import forms
-from django.forms import ModelForm, TextInput, EmailInput 
 
-from .models import User
-
-
-class UserInfoForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['name', 'email']
-        widgets = {
-            'name': TextInput(attrs={
-                'class': "form-control",
-                'style': 'max-width: 300px;',
-                'placeholder': 'Name'
-                }),
-            'email': EmailInput(attrs={
-                'class': "form-control", 
-                'style': 'max-width: 300px;',
-                'placeholder': 'Email'
-                })
-        }
+#from .models import User
+from .forms import UserInfoForm
 
 
 def index(request):
